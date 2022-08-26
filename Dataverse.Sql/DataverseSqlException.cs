74da5bb424e6cef3ddd057fa5f6a993671205135
Michael Seeger <michael_seeger@gmx.de>
2022-08-26 13:48:25 +0200

    File added
    DataverseSqlException.cs

---
+++ Dataverse.Sql/DataverseSqlException.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+using System;
+using MarkMpn.Sql4Cds.Engine;
+
+namespace Dataverse.Sql
+{
+    public class DataverseSqlException : Sql4CdsException
+    {
+        public DataverseSqlException(string message) : base(message)
+        {
+        }
+
+        public DataverseSqlException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
